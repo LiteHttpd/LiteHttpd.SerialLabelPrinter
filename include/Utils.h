02e631f6b51d5cba7ad1e02b53b0e084ebaa26f6
WuChang <3142324836@qq.com>
2024-06-12 17:56:30 +0800

    Add CMakeLists

---
+++ include/Utils.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include <string>
+
+namespace utils {
+	int getDevKitMajorVersion();
+	int getDevKitMinorVersion();
+	int getDevKitPatchVersion();
+
+	const std::string getDevKitVersionStr();
+}
---
+++ source/Utils.cpp
@@ -0,0 +1,21 @@
+﻿#include <Utils.h>
+
+namespace utils {
+	int getDevKitMajorVersion() {
+		return PROJECT_VERSION_MAJOR;
+	}
+
+	int getDevKitMinorVersion() {
+		return PROJECT_VERSION_MINOR;
+	}
+
+	int getDevKitPatchVersion() {
+		return PROJECT_VERSION_PATCH;
+	}
+
+	const std::string getDevKitVersionStr() {
+		return std::to_string(getDevKitMajorVersion()) + "."
+			+ std::to_string(getDevKitMinorVersion()) + "."
+			+ std::to_string(getDevKitPatchVersion());
+	}
+}
